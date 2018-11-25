537b5ce2be79c14c8e98aa24740c1738f5923d30
akenoq <akenoq@yandex.ru>
2018-11-25 14:45:16 +0300

    phone book begin

---
+++ urok_8/code.js
@@ -0,0 +1,26 @@
+"use strict";
+
+window.onload = function () {
+    let editName = document.getElementById("editName");
+    let editPhone = document.getElementById("editPhone");
+    let addBtn = document.getElementById("addBtn");
+
+    function getAllRecords() {
+        let bookFromStorage = localStorage.getItem("book");
+        return JSON.parse(bookFromStorage);
+    }
+
+    function addRecord(record) {
+        let bookMas = getAllRecords();
+        bookMas.push(record);
+        localStorage.setItem("book", JSON.stringify(bookMas))
+    }
+
+    addBtn.onclick = function () {
+        let record = {
+            name: editName.value,
+            phone: editPhone.value
+        };
+        addRecord(record);
+    }
+};
