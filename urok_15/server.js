3347989cb47327470ed0aa31e7ce553704828516
akenoq <akenoq@yandex.ru>
2019-02-17 16:51:55 +0300

    urok_15

--- urok_15/server.js
+++ urok_15/server.js
@@ -17,17 +17,64 @@ let users = [];
  */
 app.post('/api/register', function (request, response) {
     let dataString = "";
+
     request.on('data', (data) => {
         dataString += data;
-    }).on('end', () => {
+    });
+
+    request.on('end', () => {
 
         let dataObj = JSON.parse(dataString);
-        users.push(dataObj);
+
+        // dataObj.login уже есть в массиве?
+        let flag = 0;
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].login === dataObj.login) {
+                flag += 1;
+            }
+        }
+
+        if (flag === 0) {
+            users.push(dataObj);
+            response.end("USER ADDED");
+        } else {
+            response.end("USER ALREADY EXISTS");
+        }
 
         console.log("_____________");
         console.log(users);
+    })
+});
 
-        response.end("USER ADDED");
+app.post('/api/login', function (request, response) {
+    let dataString = "";
+
+    request.on('data', (data) => {
+        dataString += data;
+    });
+
+    request.on('end', () => {
+        let dataObj = JSON.parse(dataString);
+
+        // проверяем пару логин/пароль
+        let flag = 0;
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].login === dataObj.login &&
+                users[i].password === dataObj.password) {
+                flag += 1;
+                break;
+            }
+        }
+
+        // есть ли совпадение
+        if (flag === 0) {
+            response.end('USER NOT FOUND')
+        } else {
+            response.end(JSON.stringify({
+                status: "OK",
+                authString: dataObj.login + '_' + dataObj.password
+            }))
+        }
     })
 });
 
